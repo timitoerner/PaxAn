a8c196445d5e55808896df186558ab3eccc7b5b9
timitoerner <83765634+timitoerner@users.noreply.github.com>
2023-08-31 22:00:10 +0200

    News überarbeitet

--- app.js
+++ app.js
@@ -12,3 +12,25 @@ function menuClick()
         menu.style.animationName = "menuAnimationOut";
     }
 }
+
+
+function getTime()
+{
+    const nowDate = new Date();
+    const second = nowDate.getSeconds().toString();
+    const minute = nowDate.getMinutes().toString();
+    const hour = nowDate.getHours().toString();
+    const day = nowDate.getDate().toString();
+    const month = (nowDate.getMonth() + 1).toString();
+    const year = nowDate.getFullYear().toString();
+
+    let dateString = hour + ":" + minute + ":" + second + " " + day + "." + month + "." + year;
+
+    console.log(dateString);
+    
+    
+    for (let i = 0; i < document.getElementsByClassName("article-date").length; i++) {
+        let element = document.getElementsByClassName("article-date")[i];
+        element.innerHTML = dateString;
+    }
+}
